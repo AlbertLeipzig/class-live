17d6f560f1e4077610405b5d6d240b3382fae09e
AlbertLeipzig <albertclemvill@gmail.com>
2023-09-12 09:12:50 +0200

    added copy_destructuring

---
+++ copy_destructuring/destructuring.js
@@ -0,0 +1,13 @@
+const instrument1 = {
+  type: 'violin',
+  size: '3/4',
+  shipped: new Date(),
+  maker: {
+    type: 'luthier',
+    name: 'Mark Gibbons',
+    address: {
+      street: 'Main Street',
+      city: 'Main City',
+    },
+  },
+};
--- nested_arrays_objects/nested.js
+++ nested_arrays_objects/nested.js
@@ -1,4 +1,12 @@
-import fridgeData from './fridgeData.json' assert { type: 'json' };
+/* import fridgeData from './fridgeData.json' assert { type: 'json' }; */
+
+// objects in objects
+
+// objects in arrays
+
+// arrays in objects
+
+// arrays in arrays
 
 // manufacturer : X             ["samsung", "lg", "whirlpool"]
 // fridges color : X            ["stainless steel", "black", "white"]
@@ -7,3 +15,58 @@ import fridgeData from './fridgeData.json' assert { type: 'json' };
 // min temperature im fridge is at least 35
 // average consumption is max 150 watts
 // average rating is 3.5 or higher
+
+// test values
+
+const value1 = '150watt';
+const value2 = 'watt 150';
+const value3 = '123 123';
+
+// 1. Methode => parseFloat()
+
+const printParsedValue = (rawInput) => {
+  const value = parseFloat(rawInput);
+  console.log(value);
+};
+
+printParsedValue(value1);
+printParsedValue(value2);
+printParsedValue(value3);
+
+// 2. Methode => split(" ")
+
+const printSplittedValue = (rawInput) => {
+  const numericValues = rawInput.split(' ').filter((item) => {
+    const parsed = parseInt(Number(item));
+    return parsed;
+  });
+  console.log(numericValues);
+};
+
+printSplittedValue(value1);
+printSplittedValue(value2);
+printSplittedValue(value3);
+
+/* const getNumberFromString = (string) => {
+    const characterArray = Array.from(string);
+    let number = '';
+  
+    for (const char of characterArray) {
+      if (!isNaN(parseInt(char))) {
+        number += char;
+      } else {
+        break;
+      }
+    }
+    return number;
+  }; */
+/* const getNumberFromString = (string) => {
+  const number = Array.from(string)
+    .filter((char) => !isNaN(parseInt(char)))
+    .join('');
+  return number;
+}; */
+
+//console.log(result1);
+//console.log(result2);
+//console.log(result3);
